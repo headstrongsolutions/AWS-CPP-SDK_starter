11db623b90d5a3efc6fb33f2cdbb158d5440dbf1
Chris Morse <cgmorse@qinetiq.com>
2020-11-10 16:43:49 +0000

    S3 test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
 #include <aws/core/Aws.h>
+#include <aws/s3/S3Client.h>
+#include <aws/s3/model/Bucket.h>
+#include <awsdoc/s3/s3_examples.h>
 
-int main() {
-    std::cout << "Testing main stuff" << std::endl;
+bool AwsDoc::S3::ListBuckets()
+{
+    Aws::S3::S3Client s3_client;
+    Aws::S3::Model::ListBucketsOutcome outcome = s3_client.ListBuckets();
 
+    if (outcome.IsSuccess())
+    {
+        std::cout << "Bucket names:" << std::endl << std::endl;
+
+        Aws::Vector<Aws::S3::Model::Bucket> buckets =
+            outcome.GetResult().GetBuckets();
+
+        for (Aws::S3::Model::Bucket& bucket : buckets)
+        {
+            std::cout << bucket.GetName() << std::endl;
+        }
+
+        return true;
+    }
+    else
+    {
+        std::cout << "Error: ListBuckets: "
+            << outcome.GetError().GetMessage() << std::endl;
+
+        return false;
+    }
+}
+
+int main()
+{
     Aws::SDKOptions options;
     Aws::InitAPI(options);
     {
-        // make your SDK calls here.
+        if (!AwsDoc::S3::ListBuckets())
+        {
+            return 1;
+        }
     }
     Aws::ShutdownAPI(options);
+
     return 0;
 }
