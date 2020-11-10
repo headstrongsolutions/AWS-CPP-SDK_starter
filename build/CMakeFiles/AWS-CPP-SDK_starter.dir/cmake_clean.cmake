fe12a5def4d033a975e7f017548786cfed2bc3c2
Chris Morse <cgmorse@qinetiq.com>
2020-11-10 15:19:29 +0000

    Supporting structure for succesfully CMake

---
+++ build/CMakeFiles/AWS-CPP-SDK_starter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AWS-CPP-SDK_starter"
+  "AWS-CPP-SDK_starter.pdb"
+  "CMakeFiles/AWS-CPP-SDK_starter.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AWS-CPP-SDK_starter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
